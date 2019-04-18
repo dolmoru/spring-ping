748d2f71b11f858367c879a2c30d22864cd80d9f
Scherbakov Alexey <scherbakov_a@it-nkoadm.promis.local>
2019-04-18 16:56:11 +0300

    Initial

---
+++ src/main/java/com/example/spring/PingController.java
@@ -0,0 +1,22 @@
+package com.example.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+import java.util.Objects;
+
+@Controller
+public class PingController {
+    @GetMapping
+    public String index(@RequestParam(name="req", required=false) String req, Map<String, Object> model) {
+        if (Objects.equals(req, "ping")) {
+            model.put("some", "pong");
+        } else {
+            model.put("some", "Welcome to Spring");
+        }
+        return "index";
+    }
+}
+
